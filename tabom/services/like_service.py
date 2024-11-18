00853b5137bfc1a0554f653106d569bc8f3ce5fa
mountain-kangkang <rngl10@naver.com>
2024-11-18 11:19:28 +0900

    좋아요 취소 및 게시글 단건 조회 기능 구현

--- tabom/services/like_service.py
+++ tabom/services/like_service.py
@@ -8,3 +8,8 @@ def do_like(user_id: int, article_id: int) -> Like:
     # like_exists = Like.objects.filter(user_id=user_id, article_id=article_id).exists()
 
     return Like.objects.create(user_id=user_id, article_id=article_id)
+
+
+def undo_like(user_id: int, article_id: int) -> None:
+    # 삭제할 데이터가 없어도 아무일도 일어나지 않는다
+    Like.objects.filter(user_id=user_id, article_id=article_id).delete()
--- tabom/tests/test_like_service.py
+++ tabom/tests/test_like_service.py
@@ -3,8 +3,8 @@ from logging import raiseExceptions
 from django.db import IntegrityError
 from django.test import TestCase
 
-from tabom.models import Article, User
-from tabom.services.like_service import do_like
+from tabom.models import Article, Like, User
+from tabom.services.like_service import do_like, undo_like
 
 
 class TestLikeService(TestCase):
@@ -33,3 +33,45 @@ class TestLikeService(TestCase):
         # Exception 이 발생하면 통과, 아무일도 안 일어나면 AssertionError 를 일으킨다
         with self.assertRaises(IntegrityError):
             like2 = do_like(user.id, article.id)
+
+    def test_it_should_raise_exception_when_like_an_user_does_not_exist(self) -> None:
+        # Given
+        invalid_user_id = 9988
+        article = Article.objects.create(title="test_title")
+
+        # Expect
+        with self.assertRaises(IntegrityError):
+            do_like(invalid_user_id, article.id)
+
+    def test_it_should_raise_exception_when_like_an_article_does_not_exist(self) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        invalid_article_id = 9988
+
+        # Expect
+        with self.assertRaises(IntegrityError):
+            do_like(user.id, invalid_article_id)
+
+    def test_like_count_should_increase(self) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+
+        # When
+        do_like(user.id, article.id)
+
+        # Then
+        result_article = Article.objects.get(id=article.id)
+        self.assertEqual(1, result_article.like_set.count())
+
+    def test_a_user_can_undo_like(self) -> None:
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+        like = Like.objects.create(user=user, article=article)
+
+        undo_like(user.id, article.id)
+
+        with self.assertRaises(Like.DoesNotExist):
+            Like.objects.get(id=like.id)
+
+        # Then 절에서 DoesNotExist 로 검사하는거 말고 다른 방법이 있지 않을까?
